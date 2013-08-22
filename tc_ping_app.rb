480de212ac1ad9773946e5b93dc83a98b65012e4
andreasmaier <andreas.maier@gmail.com>
2013-08-21 22:36:53 -0700

    first done version

--- tc_ping_app.rb
+++ tc_ping_app.rb
@@ -10,21 +10,21 @@ require 'redis'
 
 scheduler = Rufus::Scheduler.start_new
 some_var = 1
+sg_username = sg_password = rds_host = rds_port = rds_password = ''
 
-credentials = sg_username = sg_password = rds_host = rds_port = rds_password = ''
-if !ENV['VCAP_SERVICES'].blank?
+unless ENV['VCAP_SERVICES'].blank?
   JSON.parse(ENV['VCAP_SERVICES']).each do |k,v|
-    if !k.scan("sendgrid").blank?
-      credentials = v.first.select {|k1,v1| k1 == "credentials"}["credentials"]
-      sg_username = credentials["username"]
-      sg_password = credentials["password"]
+    unless k.scan('sendgrid').blank?
+      credentials = v.first.select {|k1,v1| k1 == 'credentials'}['credentials']
+      sg_username = credentials['username']
+      sg_password = credentials['password']
     end
 
-    if !k.scan("rediscloud").blank?
-      credentials = v.first.select {|k1,v1| k1 == "credentials"}["credentials"]
-      rds_host = credentials["hostname"]
-      rds_port = credentials["port"]
-      rds_password = credentials["password"]
+    unless k.scan('rediscloud').blank?
+      credentials = v.first.select {|k1,v1| k1 == 'credentials'}['credentials']
+      rds_host = credentials['hostname']
+      rds_port = credentials['port']
+      rds_password = credentials['password']
     end
   end
 end
@@ -42,40 +42,11 @@ Mail.defaults do
 end
 
 scheduler.every '1m' do
-  #response = Nokogiri::HTML(open('http://www.techcrunch.com'))
-  #
-  #results = []
-  #response.css('.left-container .post .headline a').each do |item|
-  #  if item.text =~ /(Disrupt|Hackathon|Jimdo)/
-  #    results << item
-  #  end
-  #end
-  #
-  #unless results.empty?
-  #  redis = Redis.new(host: rds_host, port: rds_port, password: rds_password)
-  #
-  #  results.each do |item|
-  #    # if id is not in db
-  #    unless redis.get(item["id"])
-  #      Mail.deliver do
-  #        to 'andreas.maier@gmail.com'
-  #        from 'token@tcping.com'
-  #        subject 'Event triggered on Hackernews'
-  #        body "<div>Keywords were found in the news with the title</div><div>#{item.text.strip}</div><a href=#{item["href"]}>link</a>"
-  #      end
-  #    end
-  #  end
-  #end
-  #
-  #some_var = some_var + 1
-end
-
-get '/' do
   response = Nokogiri::HTML(open('http://www.techcrunch.com'))
 
   results = []
   response.css('.left-container .post .headline a').each do |item|
-    if item.text =~ /(Disrupt|Hackathon|Jimdo)/
+    if item.text =~ /(Disrupt|Hackathon)/
       results << item
     end
   end
@@ -84,7 +55,6 @@ get '/' do
     redis = Redis.new(host: rds_host, port: rds_port, password: rds_password)
 
     results.each do |item|
-      # if id is not in db
       unless redis.get(item["id"])
         Mail.deliver do
           to 'andreas.maier@gmail.com'
@@ -99,6 +69,8 @@ get '/' do
   end
 
   some_var = some_var + 1
+end
 
+get '/' do
   "Hello world! #{some_var} new \n\n"
 end
